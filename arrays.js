71dfd58f56dcd5436ac00b705fd667c92a4297c0
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-06-16 01:34:03 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -66,3 +66,9 @@ function destructivelyRemoveElementFromEndOfArray (chocolateBars) {
   return chocolateBars
 
 }
+
+function removeElementFromEndOfArray(chocolateBars) {
+
+  return chocolateBars.slice(0,chocolateBars.length - 1)
+
+}
