5b4510cb6a3b85d55b4448c60f13b231e2555b7a
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-06-16 00:58:35 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -4,3 +4,21 @@ var chocolateBars = [
   'kit kat',
   'skittles'
 ]
+
+function addElementToBeginningOfArray(chocolateBars, newCandy) {
+  
+  var newCandy = 'three musketeers'
+  
+  var newChocolateBars = [newCandy, ...chocolateBars]
+  
+  return newChocolateBars
+}
+
+function destructivelyAddElementToBeginningOfArray(chocolateBars, newCandy) {
+
+  var newCandy = 'three musketeers'
+
+  chocolateBars.unshift(newCandy)
+
+  return chocolateBars
+}
